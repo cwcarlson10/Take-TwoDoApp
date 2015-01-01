8d6317118d76870ff3bbddbdf12cc5527c057cad
Chris Carlson <cwcarlson10@gmail.com>
2015-01-01 16:04:30 -0700

    added user model through devise

--- Gemfile
+++ Gemfile
@@ -34,6 +34,7 @@ group :development, :test do
   gem 'binding_of_caller'
   gem 'spring'
 end
+gem 'devise'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users, :skip => [:sessions]
+  as :user do
+    get 'signin' => 'devise/sessions#new', :as => :new_user_session
+    post 'signin' => 'devise/sessions#create', :as => :user_session
+    delete 'signout' => 'devise/sessions#destroy', :as => :destroy_user_session
+  end
   root 'tasks#index'
   resources :tasks
 
