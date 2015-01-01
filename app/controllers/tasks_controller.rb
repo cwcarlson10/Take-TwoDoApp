2007e2838acf43d3e24901df82ac60b8d8d4066f
Chris Carlson <cwcarlson10@gmail.com>
2015-01-01 14:50:19 -0700

    sorted the tasks by completeion and priority

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,7 +2,7 @@ class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
 
   def index
-    @tasks = Task.all
+    @tasks = Task.all.order(:is_completed, priority: :asc)
   end
 
   def show
@@ -11,24 +11,26 @@ class TasksController < ApplicationController
   def new
     @task = Task.new
     @status = ["Yes", "No"]
+    @priority_list = ["1 - High", "1 - Medium", "3 - Low"]
   end
 
   def create
     @task = Task.create(task_params)
     if @task.save
-      redirect_to @task
+      redirect_to tasks_path
     else
       render :new
     end
   end
 
   def edit
+    @priority_list = ["1 - High", "2 - Medium", "3 - Low"]
   end
 
   def update
     @task.update_attributes(task_params)
     if @task.save
-      redirect_to @task
+      redirect_to tasks_path
     else
       render :edit
     end
